7bedafbb06c76f3d88f2a3b746af0ff677092b26
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2022-07-08 15:41:32 +0700

    Add nix-direnv to enable nix-shell caching and avoid garbage collector for direnv

--- systems/development/direnv.nix
+++ systems/development/direnv.nix
@@ -1,11 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.unstable.direnv ];
+  environment.systemPackages = with pkgs; [ unstable.direnv nix-direnv ];
+  environment.pathsToLink = [ "/share/nix-direnv" ];
+
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
 
   home-manager.sharedModules = [{
     xdg.configFile = {
       "direnv/direnv.toml".source = ../config/direnv/direnv.toml;
+      "direnv/direnvrc".source = ../config/direnv/direnvrc;
     };
   }];
 }
