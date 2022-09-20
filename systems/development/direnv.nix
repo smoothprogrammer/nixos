c89f08b7d4a9f247d08725846a10ca583a1b8808
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2022-09-20 14:54:22 +0700

    Disable nix-direnv

--- systems/development/direnv.nix
+++ systems/development/direnv.nix
@@ -12,7 +12,9 @@
   home-manager.sharedModules = [{
     xdg.configFile = {
       "direnv/direnv.toml".source = ../config/direnv/direnv.toml;
-      "direnv/direnvrc".source = ../config/direnv/direnvrc;
+      # I'm disabling nix-direnv because it can't invalidate the cache
+      # when buildGoModule fields like name, version, and src updated.
+      # "direnv/direnvrc".source = ../config/direnv/direnvrc;
     };
   }];
 }
