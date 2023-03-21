faebc4def61bdc8dd467c097da2ae2680e1e0f88
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-03-22 06:46:31 +0700

    Remove firefox view

--- conf/firefox/user-overrides.js
+++ conf/firefox/user-overrides.js
@@ -3,5 +3,6 @@
 user_pref("layers.acceleration.disabled", true);
 
 user_pref("signon.rememberSignons", false);
+user_pref("browser.tabs.firefox-view", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("identity.fxaccounts.enabled", false);
