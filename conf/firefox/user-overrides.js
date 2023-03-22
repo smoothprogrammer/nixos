8c8651e88dd2b75f988e4e8b21b09bb0f132dc4d
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-03-22 10:12:28 +0700

    Enable firefox keyword

--- conf/firefox/user-overrides.js
+++ conf/firefox/user-overrides.js
@@ -1,10 +1,9 @@
 // user-overrides.js for arkenfox user.js version 110
 
+user_pref("keyword.enabled", true);
 user_pref("layers.acceleration.disabled", true);
-
 user_pref("signon.rememberSignons", false);
 user_pref("browser.tabs.firefox-view", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("identity.fxaccounts.enabled", false);
-
 user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
