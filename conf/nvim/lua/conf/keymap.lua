9d3881a9d511dcc52504e83fd735424c0391528c
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-04-02 07:13:41 +0700

    Remap nvim keymap

--- conf/nvim/lua/conf/keymap.lua
+++ conf/nvim/lua/conf/keymap.lua
@@ -10,23 +10,15 @@ local imap = function(lhs, rhs)
   vim.keymap.set('i', lhs, rhs, { silent = true })
 end
 
-vim.g.mapleader = ';'
+vim.g.mapleader = ' '
 
 map('.', ';')
-map(' ', '.')
+map('<Leader>.', '.')
 imap('jj', '<Esc>')
-nmap('<Leader>w', ':w<CR>')
-nmap('<Leader>q', ':q<CR>')
+nmap(';w', ':w<CR>')
+nmap(';q', ':q<CR>')
 nmap('<CR>', 'o<Esc>')
 nmap('<S-CR>', 'O<Esc>')
-nmap('<M-h>', '<C-w>h')
-nmap('<M-j>', '<C-w>j')
-nmap('<M-k>', '<C-w>k')
-nmap('<M-l>', '<C-w>l')
-nmap('<M-H>', '<C-w>H')
-nmap('<M-J>', '<C-w>J')
-nmap('<M-K>', '<C-w>K')
-nmap('<M-L>', '<C-w>L')
 map('<Left>', '<Nop>')
 map('<Down>', '<Nop>')
 map('<Up>', '<Nop>')
@@ -35,13 +27,13 @@ map('<PageUp>', '<Nop>')
 map('<PageDown>', '<Nop>')
 
 local telescope = require('telescope.builtin')
-nmap('<C-f>', telescope.find_files)
-nmap('<C-s>', telescope.live_grep)
+nmap('<C-p>', telescope.find_files)
+nmap('<C-f>', telescope.live_grep)
 nmap('<C-b>', ':Telescope file_browser<CR>')
 nmap('<C-x>', ':Telescope manix<CR>')
 
-nmap('<C-p>', vim.diagnostic.goto_prev)
-nmap('<C-n>', vim.diagnostic.goto_next)
+nmap('[d', vim.diagnostic.goto_prev)
+nmap(']d', vim.diagnostic.goto_next)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -52,6 +44,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gr', telescope.lsp_references, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<leader>ds', telescope.lsp_document_symbols, bufopts)
+    vim.keymap.set('n', '<leader>ws', telescope.lsp_workspace_symbols, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 
