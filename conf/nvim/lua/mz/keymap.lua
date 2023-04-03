04d8d4dafcc28379cafc11b62db2fd3e3ba2bfc8
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-04-03 11:34:48 +0700

    Add keymap for visual paste and center scroll

--- conf/nvim/lua/mz/keymap.lua
+++ conf/nvim/lua/mz/keymap.lua
@@ -3,16 +3,20 @@ local lib = require('mz.lib')
 local map = lib.keymapper('', { silent = true })
 local nmap = lib.keymapper('n', { silent = true })
 local imap = lib.keymapper('i', { silent = true })
+local xmap = lib.keymapper('x', { silent = true })
 
 vim.g.mapleader = ' '
 
 map('.', ';')
 map('<Leader>.', '.')
+xmap('<Leader>p', '"_dP')
 imap('jj', '<Esc>')
 nmap(';w', ':w<CR>')
 nmap(';q', ':q<CR>')
 nmap('<CR>', 'o<Esc>')
 nmap('<S-CR>', 'O<Esc>')
+nmap('<C-d>', '<C-d>zz')
+nmap('<C-u>', '<C-u>zz')
 
 map('<Left>', '<Nop>')
 map('<Down>', '<Nop>')
