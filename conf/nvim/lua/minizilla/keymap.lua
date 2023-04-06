b5f500fb4d78a9c8ed9666252e49282b4bff5645
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-04-06 19:51:55 +0700

    nvim: remap for moving line in visual mode

--- conf/nvim/lua/minizilla/keymap.lua
+++ conf/nvim/lua/minizilla/keymap.lua
@@ -1,21 +1,30 @@
 local map = Lib.keymapper('', { silent = true })
 local nmap = Lib.keymapper('n', { silent = true })
 local imap = Lib.keymapper('i', { silent = true })
-local xmap = Lib.keymapper('x', { silent = true })
+local vmap = Lib.keymapper('v', { silent = true })
 
 vim.g.mapleader = ' '
 
 imap('jj', '<Esc>')
 map('.', ';')
+
 map('<Leader>.', '.')
-xmap('<Leader>p', '"_dP')
+vmap('<Leader>p', '"_dP')
 nmap('<Leader>cd', function() vim.api.nvim_set_current_dir(vim.fn.expand('%:h')) end)
+
 nmap(';w', ':w<CR>')
 nmap(';q', ':q<CR>')
+
 nmap('<CR>', 'o<Esc>')
 nmap('<S-CR>', 'O<Esc>')
+
 nmap('<C-d>', '<C-d>zz')
 nmap('<C-u>', '<C-u>zz')
+
+vmap('J', [[:m '>+1<CR>gv=gv]])
+vmap('K', [[:m '<-2<CR>gv=gv]])
+
+nmap('J', 'mzJ`z')
 
 map('<Left>', '<Nop>')
 map('<Down>', '<Nop>')
