7d0b419b9f3cac231e5a2bfa149313f0fc10151a
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2023-10-08 08:53:58 +0700

    lib/mkDarwin: refactor orgs

--- lib/mkdarwin.nix
+++ lib/mkdarwin.nix
@@ -1,6 +1,6 @@
 { stateVersion
-, darwin
 , nixpkgs
+, darwin
 , home-manager
 , ...
 }: darwinConfigurations:
@@ -16,39 +16,17 @@
 builtins.mapAttrs
   (hostname: settings: darwin.lib.darwinSystem {
     inherit (settings) system;
+    specialArgs = {
+      inherit stateVersion hostname;
+      inherit (settings) user;
+    };
     modules = [
       home-manager.darwinModules.default
 
       ({ config, lib, pkgs, home-manager, ... }: {
-        networking.hostName = hostname;
-        time.timeZone = "Asia/Jakarta";
-
         nixpkgs.overlays = [ (import ./overlays.nix) ];
-        nixpkgs.config.allowUnfree = true;
-
         nix.registry.nixpkgs.flake = nixpkgs;
         nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
-        nix.gc = {
-          automatic = true;
-          options = "--delete-older-than 14d";
-        };
-        nix.extraOptions = ''
-          experimental-features = nix-command flakes
-          warn-dirty = false
-          keep-outputs = true
-          keep-derivations = true
-        '';
-
-        users.users.${settings.user} = {
-          createHome = true;
-          home = "/Users/${settings.user}";
-        };
-
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.${settings.user} = {
-          home.stateVersion = stateVersion;
-        };
       })
 
       ../machines/${settings.machine}
--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -1,12 +1,56 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }@args:
 
 {
+  # Managed daemon service.
   services.nix-daemon.enable = true;
 
+  # Hostname.
+  networking.hostName = args.hostname;
+
+  # Timezone.
+  time.timeZone = "Asia/Jakarta";
+
+  # Allow unfree packages.
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto garbage collector.
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 14d";
+  };
+
+  # Enable new nix cli and flakes.
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+    warn-dirty = false
+    keep-outputs = true
+    keep-derivations = true
+  '';
+
+  # Single user.
+  users.users.${args.user} = {
+    createHome = true;
+    home = "/Users/${args.user}";
+  };
+
+  # A must have home-manager config.
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  # Single user home-manager.
+  home-manager.users.${args.user} = {
+    home.stateVersion = args.stateVersion;
+  };
+
+  # Default programs.
+  programs.zsh.enable = true;
+
+  # Default system packages.
   environment.systemPackages = with pkgs; [
     vim
   ];
 
+  # We still needs homebrew since nixpkgs support for darwin is lacking.
   homebrew.enable = true;
   homebrew.casks = [
     "dbeaver-community"
