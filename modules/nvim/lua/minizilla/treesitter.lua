f4e2d359e479b1915e929d988532c6f8714833d5
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2024-11-03 16:33:36 +0700

    nvim(treesitter): add indent

--- modules/nvim/lua/minizilla/treesitter.lua
+++ modules/nvim/lua/minizilla/treesitter.lua
@@ -3,6 +3,9 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+    enable = true,
+  },
   incremental_selection = {
     enable = true,
     keymaps = {
